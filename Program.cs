df77f895b0d3dc87ad996cf69d89005d208d8264
Sarah Beth Langley <sarahbethdoescode@gmail.com>
2022-11-18 16:00:30 -0600

    working phase two

--- Program.cs
+++ Program.cs
@@ -9,11 +9,25 @@ namespace guessing_game
         static void Main(string[] args)
         {
 
-
+            int secretNum = 42;
 
             Console.WriteLine("Guess the secret number: ");
+            bool foundNum = false;
+
             string userGuess = Console.ReadLine();
             int parsedGuess = int.Parse(userGuess);
+
+            if (parsedGuess == secretNum)
+            {
+                Console.WriteLine("Yep, that's the number");
+                foundNum = true;
+            }
+            else
+            {
+                Console.WriteLine("Nah that ain't it.");
+            }
+
+
             Console.WriteLine(userGuess);
 
         }
